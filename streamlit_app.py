4da61bc35ad4035f6b0a94b29f0a83a1296e0721
Akanksha Mate <85989430+akankshamate@users.noreply.github.com>
2022-09-02 15:37:37 +0530

    Update streamlit_app.py
    Added breakfast menu header and 4 times text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
-streamlit.title("Hello User!!!")
-streamlit.header("Programming Languages:")
-streamlit.text("C")
-streamlit.text("C++")
-streamlit.text("Java")
-streamlit.text("Python")
+streamlit.title("My Mom's New Healty Diner!!!")
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
