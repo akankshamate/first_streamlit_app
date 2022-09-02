a2592e32f3965786a3d91219290f3dff13952107
Akanksha Mate <85989430+akankshamate@users.noreply.github.com>
2022-09-02 15:28:10 +0530

    Update streamlit_app.py
    Added header and some text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title("Hello User!!!")
+streamlit.header("Programming Languages:")
+streamlit.text("C")
+streamlit.text("C++")
+streamlit.text("Java")
+streamlit.text("Python")
